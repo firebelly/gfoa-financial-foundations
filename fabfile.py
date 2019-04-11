3c4df28ed9d975579f14b4044335d9b1a1a265f6
poopsplat <matt.m.soria@gmail.com>
2019-04-11 16:29:29 -0500

    Updating fabfile

--- fabfile.py
+++ fabfile.py
@@ -1,20 +1,20 @@
 from fabric.api import *
 import os
 
-env.hosts = ['staging.starthere.com']
-env.user = 'starthereuser'
-env.path = '~/Path/to/site'
-env.remotepath = '/path/to/site'
-env.git_branch = 'staging'
+env.hosts = ['gfoa-financial-foundations.firebelly.co']
+env.user = 'firebelly'
+env.path = '~/Sites/gfoa-financial-foundations'
+env.remotepath = '/home/firebelly/webapps/gfoa_financial_foundations'
+env.git_branch = 'master'
 env.warn_only = True
 env.remote_protocol = 'http'
 
-def production():
-  env.hosts = ['starthere.com']
-  env.user = 'starthereuser'
-  env.remotepath = '/path/to/site'
-  env.git_branch = 'master'
-  env.remote_protocol = 'https'
+# def production():
+#   env.hosts = ['gfoa.org']
+#   env.user = 'starthereuser'
+#   env.remotepath = '/path/to/site'
+#   env.git_branch = 'master'
+#   env.remote_protocol = 'https'
 
 def build():
   local('npx gulp --production')
